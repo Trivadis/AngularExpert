29e934d7ab09cf1dbbc427264cd9f2da41c60bb1
Francesco Leardini <piolag@gmail.com>
2022-01-05 12:08:38 +0100

    Added reactive form workspace

---
+++ reactive-forms/final/reactiveForms/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeValidators } from './Validators/employee.validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  constructor(private fb: FormBuilder) {}
+
+  form: FormGroup = this.fb.group(
+    {
+      firstname: [
+        '',
+        {
+          validators: Validators.required,
+          updateOn: 'blur',
+        },
+      ],
+      lastname: ['', Validators.required],
+      email: [
+        '',
+        [Validators.required, EmployeeValidators.emailValidator],
+        //   EmployeeValidators.checkEmailUnique(this.service),
+      ],
+    },
+    {
+      validators: EmployeeValidators.checkEmailsMatch,
+      updateOn: 'blur',
+    }
+  );
+
+  get firstname() {
+    return this.form.get('firstname');
+  }
+  get lastname() {
+    return this.form.get('lastname');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get emailConfirm() {
+    return this.form.get('emailConfirm');
+  }
+}
