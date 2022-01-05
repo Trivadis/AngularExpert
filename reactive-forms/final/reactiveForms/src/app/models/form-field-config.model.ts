cd0c9e764d6a08eaa86ce0b48154e7cf2664f623
Francesco Leardini <piolag@gmail.com>
2022-01-05 16:33:40 +0100

    Added code for dynamic forms

--- reactive-forms/final/reactiveForms/src/app/models/form-field-config.model.ts
+++ reactive-forms/final/reactiveForms/src/app/models/form-field-config.model.ts
@@ -1,7 +1,10 @@
+import { FormFieldType } from './form-field-types.enum';
+
 export interface FormFieldConfig {
   fieldId: string;
+  type: FormFieldType;
   label?: string;
-  value?: string;
+  value?: string | boolean | number;
   options?: any[];
   disabled?: boolean;
   required?: boolean;
--- reactive-forms/final/reactiveForms/src/app/services/employee.service.ts
+++ reactive-forms/final/reactiveForms/src/app/services/employee.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import { of, throwError as _throw } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { delay, map } from 'rxjs/operators';
+import { FormFieldConfig } from '../models/form-field-config.model';
+import { FormFieldType } from '../models/form-field-types.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +17,51 @@ export class EmployeeService {
       delay(1000),
       map((e) => e !== 'info@trivadis.com')
     );
+  }
+
+  getDynamicFormFields(
+    userType: 'user' | 'admin'
+  ): Observable<FormFieldConfig[]> {
+    // The condition here is just for sake of example
+    // to simulate a server providing different configs
+    if (userType === 'admin') {
+      return of([
+        {
+          fieldId: 'role',
+          type: FormFieldType.text,
+          value: 'Site admin',
+          label: 'User role',
+          disabled: true,
+        },
+        {
+          fieldId: 'onDuty',
+          type: FormFieldType.checkbox,
+          label: 'On duty',
+          value: true,
+        },
+      ]);
+    } else {
+      return of([
+        {
+          fieldId: 'role',
+          type: FormFieldType.text,
+          value: 'Client',
+          label: 'User role',
+          disabled: true,
+        },
+        {
+          fieldId: 'companyName',
+          type: FormFieldType.text,
+          label: 'Company Name',
+          value: 'Trivadis',
+          required: true,
+        },
+        {
+          fieldId: 'favTech',
+          type: FormFieldType.text,
+          label: 'Favourite technology',
+        },
+      ]);
+    }
   }
 }
