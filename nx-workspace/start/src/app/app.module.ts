d9dab4edd8662a43791d0fc431b28119d604cbe0
Francesco Leardini <piolag@gmail.com>
2022-01-06 15:54:33 +0100

    Updated started demo code

--- nx-workspace/start/src/app/app.module.ts
+++ nx-workspace/start/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
