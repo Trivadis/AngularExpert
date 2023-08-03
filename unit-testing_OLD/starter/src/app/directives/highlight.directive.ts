39e59ecd551f5e2d9aecd0d767dfb8751b20a027
Francesco Leardini <piolag@gmail.com>
2023-08-03 05:09:20 +0200

    Aded unit test start

---
+++ unit-testing_OLD/starter/src/app/directives/highlight.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({ selector: '[appHighlight]' })
+export class HighlightDirective {
+  @Input('appHighlight') highlightColor: string | undefined;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string | null) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
---
+++ unit-testing_OLD/starter/src/app/services/employee.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, of } from 'rxjs';
+import { Employee } from 'src/app/models/employee.model';
+
+@Injectable({ providedIn: 'root' })
+export class EmployeeService {
+  private employees: Employee[] = [
+    { id: 1, name: 'Thomas Gassmann', email: 'thomas.gassmann@accenture.com' },
+    {
+      id: 2,
+      name: 'Francesco Leardini',
+      email: 'francesco.leardini@accenture.com',
+    },
+  ];
+  constructor(private httpClient: HttpClient) {}
+
+  getAll(): Observable<Employee[]> {
+    return of(this.employees);
+  }
+
+  getById(id: number): Observable<Employee | undefined> {
+    const employee = this.employees.find((e) => e.id === id);
+    return of(employee);
+  }
+
+  getExampleProducts(): Observable<{ name: string }[]> {
+    return this.httpClient
+      .get<{ meta: any; products: { name: string }[] }>(
+        'https://api.predic8.de/shop/products/'
+      )
+      .pipe(map((x) => x.products));
+  }
+}
