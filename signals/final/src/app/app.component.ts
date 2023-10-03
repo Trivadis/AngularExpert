139677f91ec17d06a788c607fc43bb93911e9e52
Francesco Leardini <piolag@gmail.com>
2023-10-03 16:54:36 +0200

    General style rules

--- signals/final/src/app/app.component.ts
+++ signals/final/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, WritableSignal, inject, signal } from '@angular/core';
+import { Component, OnInit, WritableSignal, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BeerService } from './services/beer.service';
@@ -18,8 +18,19 @@ export class AppComponent implements OnInit {
   title = 'Signals Demo';
   favCount: WritableSignal<number> = signal(0);
   availableBeers$?: Observable<BeerResponse[]>;
-  
   beerService = inject(BeerService);
+  shippingCost = '';
+
+  constructor() {
+    effect(() => {
+      console.log(`The count is: ${this.favCount()})`);
+      if (this.favCount() > 3) {
+        this.shippingCost = 'Free!';
+      } else {
+        this.shippingCost = '30 CHF';
+      }
+    });
+  }
   
   ngOnInit() {
     this.availableBeers$ = this.beerService.getBeerList();
