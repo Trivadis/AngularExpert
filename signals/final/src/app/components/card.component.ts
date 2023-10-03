08d6af076803c16ada6fb15666d2645816aca86a
Francesco Leardini <piolag@gmail.com>
2023-10-03 16:54:24 +0200

    Added structure to cards and style

--- signals/final/src/app/components/card.component.ts
+++ signals/final/src/app/components/card.component.ts
@@ -1,16 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 import { BeerResponse } from '../model/beer.model';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
   @Input() beer?: BeerResponse;
+
+  isLiked = false;
+
+  like() {
+    this.isLiked = !this.isLiked;
+  }
 
 }
